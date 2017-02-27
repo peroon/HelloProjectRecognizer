c97aea86e02bf5e5ff14a6a987b8f965bdb91df1
peroon <fromjpn@gmail.com>
2017-02-27 19:17:08 +0900

    カラーコードをcv2用にタプルに変換する関数を追加

---
+++ code/color.py
@@ -0,0 +1,20 @@
+# RGB
+def color_code_to_rbg_tuple(color_code):
+    code_r = color_code[1:3]
+    code_g = color_code[3:5]
+    code_b = color_code[5:7]
+    r = int(code_r, 16)
+    g = int(code_g, 16)
+    b = int(code_b, 16)
+    return r, g, b
+
+
+# BGR for cv2
+def color_code_to_bgr_tuple(color_code):
+    rgb = color_code_to_rbg_tuple(color_code)
+    return rgb[2], rgb[1], rgb[0]
+
+
+if __name__ == '__main__':
+    col = color_code_to_rbg_tuple("#FF0000")
+    print(col)
