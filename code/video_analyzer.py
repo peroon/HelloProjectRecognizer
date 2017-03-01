4a4c72e44a5429006e298a6fe1ae6a18a8ac6266
peroon <fromjpn@gmail.com>
2017-03-01 21:43:20 +0900

    ビデオを入力として顔認識・顔識別を実行する

--- code/video_analyzer.py
+++ code/video_analyzer.py
@@ -3,20 +3,38 @@
 """動画を分析して結果を返す"""
 
 from image_analyzer import ImageAnalyzer
-
+import imageio
+import cv2
+import time
 
 class VideoAnalyzer():
     def __init__(self):
         self.image_analyzer = ImageAnalyzer()
 
-    def analyze(self, video_path):
-        # load
+    def analyze(self, video_path, output_directory):
+        # load video
+        reader = imageio.get_reader(video_path, 'ffmpeg')
+        frame_num = reader._meta['nframes']
+        print('frame num', frame_num)
 
         # analyze each frame
+        interval = 100
+        for i in range(0, frame_num, interval):
+            print(i)
+            image = reader.get_data(i)
+            temp_path = '../temp/temp.jpg'
+            imageio.imwrite(uri=temp_path, im=image)
+            result_image = self.image_analyzer.analyze(temp_path)
 
-        result = []
-        return result
+            save_path = output_directory + '{0:08d}'.format(i) + '.jpg'
+            cv2.imwrite(save_path, result_image)
 
 if __name__ == '__main__':
     video_analyzer = VideoAnalyzer()
-    pass
+    video_path = '../resources/youtube_1080p/bfoogf5i6N8.mp4'
+    output_dir =  '../temp/video_analyze_result/'
+
+    start_time = time.time()
+    video_analyzer.analyze(video_path=video_path, output_directory=output_dir)
+    end_time = time.time()
+    print('analyze time:', end_time - start_time)
