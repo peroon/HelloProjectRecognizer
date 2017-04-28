1f9cb2307aa58025155ccbe8d0a32a514fa2399f
peroon <fromjpn@gmail.com>
2017-04-28 23:51:23 +0900

    Generate bat file to make rec

--- code/dmlc_mxnet/example/image-classification/prepare.py
+++ code/dmlc_mxnet/example/image-classification/prepare.py
@@ -13,6 +13,8 @@ from tqdm import tqdm
 import data
 from constant import PROJECT_ROOT
 
+lst_output_dir = PROJECT_ROOT + '/resources/rec_files'
+
 
 def split_list(list_input, fold_num=5, fold_index=0):
     """split data for train and validation"""
@@ -70,9 +72,6 @@ def make_train_and_validation_lst():
         labels_validation += validation_labels
 
     # save
-    lst_output_dir = PROJECT_ROOT + '/resources/rec_files'
-
-    # ファイルに書き込む
     train_lst_path = lst_output_dir + '/training.lst'
     write_lst(train_lst_path, image_path_list_training, labels_training)
 
@@ -80,40 +79,24 @@ def make_train_and_validation_lst():
     write_lst(validation_lst_path, image_path_list_validation, labels_validation)
 
 
-def make_test_lst():
-
-    # 各cropごとに1ファイルにする
-    for crop_i in range(0, 4):
-        glob_path = IMAGES_ROOT + 'clf_test_224x224_{}/*'.format(crop_i)
-        image_path_list = glob.glob(glob_path)
-
-        if DEBUG:
-            image_path_list = image_path_list[:100]
-
-        labels = [0] * len(image_path_list)
-
-        # ファイルに書き込む
-        lst_path = mxnet_dir + 'test_224x224_crop_{}.lst'.format(crop_i)
-        write_lst(lst_path, image_path_list, labels)
-
-
 def make_rec():
-    im2rec_fullpath = os.path.abspath('./dmlc_mxnet/tools/im2rec.py')
-    lst_path_list = glob.glob(mxnet_dir + '*.lst')
+    im2rec_fullpath = os.path.abspath('../..//tools/im2rec.py')
+    lst_path_list = glob.glob(lst_output_dir + '/*.lst')
 
     # make bat
-    with open(mxnet_dir + 'make_rec.bat', 'w') as f:
+    with open(lst_output_dir + '/make_rec.bat', 'w') as f:
         for lst_path in lst_path_list:
             filename_without_ext = os.path.basename(lst_path).split('.')[0]
             print(filename_without_ext)
-            s = 'python {} --encoding .png {} .'.format(im2rec_fullpath, filename_without_ext)
+            s = 'python {} --encoding .jpg {} .'.format(im2rec_fullpath, filename_without_ext)
             f.write(s + '\n')
 
     print('Execute .bat to make rec file.')
-    print(mxnet_dir)
+    print(lst_output_dir)
 
 
 
 
 if __name__ == '__main__':
-    make_train_and_validation_lst()
+    # make_train_and_validation_lst()
+    make_rec()
