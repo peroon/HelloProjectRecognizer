622d9e134fd77169964aa87bcdcc8f5b278723df
peroon <fromjpn@gmail.com>
2017-05-05 23:47:24 +0900

    Enable batch processing of video download

--- code/collect/youtube_downloader.py
+++ code/collect/youtube_downloader.py
@@ -4,11 +4,13 @@ from pytube import YouTube
 from pytube import exceptions
 from pprint import pprint
 import os.path
+import glob
+from constant import PROJECT_ROOT
 
 
 def download(youtube_id, save_dir, video_size='720p'):
     if os.path.exists(save_dir + youtube_id + 'mp4'):
-        print('すでにあるのでスキップします')
+        print('SKIP because the video was already downloaded.')
     else:
         url = "http://www.youtube.com/watch?v={0}".format(youtube_id)
         print('url', url)
@@ -24,10 +26,26 @@ def download(youtube_id, save_dir, video_size='720p'):
 
         except exceptions.DoesNotExist:
             video = yt.get('mp4')
-            print('低解像度 download', youtube_id)
+            print('[exception] low res download', youtube_id)
             video.download(save_dir)
             print('done.')
 
+
+def get_already_downloaded_list():
+    lst = []
+    path_list = glob.glob(PROJECT_ROOT + '/resources/youtube/*.mp4')
+    for path in path_list:
+        filename_without_ext = os.path.basename(path).split('.')[0]
+        lst.append(filename_without_ext)
+    return lst
+
+
 if __name__ == '__main__':
-    youtube_id = 'bfoogf5i6N8'
-    download(youtube_id, save_dir='../../resources/youtube/')
+    download_list_path = './youtube_download_list.txt'
+    with open(download_list_path, 'r') as f:
+       for s in f:
+           youtube_id = s.strip()
+           download(youtube_id, save_dir='../../resources/youtube/')
+
+    # if done correctly, make list empty
+    f = open(download_list_path, 'w').close()
