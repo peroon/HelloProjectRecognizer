efaee94bde199d725e6ceb89e6d613c2622118c3
peroon <fromjpn@gmail.com>
2017-06-19 23:38:58 +0900

    Load CSV for video list view

--- code/page/list.js
+++ code/page/list.js
@@ -1,6 +1,27 @@
-console.log('hello list');
-
 var csv_path = "./data/videos.csv";
-$.getJSON(csv_path, function(obj) {
-   console.log('loaded');
-});
+var req = new XMLHttpRequest();
+req.open("get", csv_path, true);
+req.send(null);
+req.onload = function(){
+    loadCSV(req.responseText);
+}
+
+var videos = [];
+function loadCSV(str){
+    var tmp = str.split("\n");
+    tmp = tmp.slice(1);
+
+    for(var i=0;i<tmp.length;++i){
+        if(tmp[i] == ''){
+            continue;
+        }
+        videos[i] = tmp[i].split(',');
+    }
+
+    onCompleteLoad();
+}
+
+function onCompleteLoad(){
+    console.log(videos[0]);
+    console.log(videos[1]);
+}
