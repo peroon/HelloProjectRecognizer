ccfd245623015c817a62065855d31fc12046b614
peroon <fromjpn@gmail.com>
2017-08-06 02:06:00 +0900

    Get YouTube video's createdAt

--- code/youtube_api.py
+++ code/youtube_api.py
@@ -1,16 +1,26 @@
 # https://github.com/youtube/api-samples/tree/master/python
 
-from google_image_collector import get_api_key, get_cx
+import requests
+import json
+import pprint
 
+import google_api
 
 def get_video_info(video_id):
-    api_key = get_api_key()
-    cx = get_cx()
+    api_key = google_api.get_api_key()
+    url = 'https://www.googleapis.com/youtube/v3/videos?id=' + video_id + '&key=' + api_key + \
+          '&fields=items(id,snippet(title,publishedAt))&part=snippet,contentDetails,statistics'
+    r = requests.get(url)
+    json_obj = json.loads(r.text)
+    for i, item in enumerate(json_obj['items']):
+        pprint.pprint(item)
+        id = item['id']
+        published_at = item['snippet']['publishedAt']
+        title = item['snippet']['title']
+        print(id, published_at, title)
 
-    print(api_key)
-    print(cx)
 
 if __name__ == '__main__':
-    video_id = 'VA770wpLX-Q'
+    video_id = '0EwG_EJ7Aaw'
     info = get_video_info(video_id)
     print(info)
