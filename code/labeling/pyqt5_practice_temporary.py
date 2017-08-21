7c06b129547cdddd13415620382048ff74416bfb
peroon <fromjpn@gmail.com>
2017-08-21 23:45:40 +0900

    [PyQt]procedual to OOP

--- code/labeling/pyqt5_practice_temporary.py
+++ code/labeling/pyqt5_practice_temporary.py
@@ -1,13 +1,20 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        #self.setWindowIcon(QIcon('web.png'))
+        self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec_())
