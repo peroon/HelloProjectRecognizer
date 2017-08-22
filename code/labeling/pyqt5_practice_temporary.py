47031a680fdc9140419a17ecbadccc1b49889ce2
peroon <fromjpn@gmail.com>
2017-08-22 23:55:21 +0900

    [PyQt]Centering window

--- code/labeling/pyqt5_practice_temporary.py
+++ code/labeling/pyqt5_practice_temporary.py
@@ -1,18 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
-
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        #self.setWindowIcon(QIcon('web.png'))
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle('Center')
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
