1b37e954491ed93ea8e45d5c1103ce76750d7bf2
peroon <fromjpn@gmail.com>
2017-09-09 20:06:31 +0900

    Introduction of button with image

--- code/labeling/LabelButtons.py
+++ code/labeling/LabelButtons.py
@@ -1,5 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QSizePolicy, QLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QSizePolicy, QToolButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
+
+
+import constant
 
 IMAGE_BUTTON_SIZE = 200
 
@@ -10,28 +15,28 @@ class LabelButtons(QWidget):
 
         vertical = QVBoxLayout()
 
-        btn1 = QPushButton("Button 1", self)
+        btn1 = QPushButton('', self)
         btn1.move(30, 50)
-        btn1.resize(200, 200)
         btn1.setFixedSize(200, 200)
         btn1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        icon_path = constant.PROJECT_ROOT + '/docs/images/face_icon/0000.jpg'
+        btn1.setIcon(QIcon(icon_path))
+        btn1.setIconSize(QSize(180, 180))
 
         btn2 = QPushButton("Button 2", self)
         btn2.move(150, 50)
 
-        btn1.clicked.connect(self.on_press_button)
+        btn1.clicked.connect(lambda: self.on_press_button(1))
         btn2.clicked.connect(self.on_press_button)
 
-        #vertical.setSizeConstraint(QLayout.SetFixedSize)
         vertical.addWidget(btn1)
         vertical.addWidget(btn2)
 
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setLayout(vertical)
 
-    def on_press_button(self):
+    def on_press_button(self, idol_id):
         sender = self.sender()
-        print(sender.text())
+        print(idol_id)
 
 
 def test():
